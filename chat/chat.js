95dd7c1c63505cc0d33c082d09543bf7afbe6333
crossar <41621244+crossar@users.noreply.github.com>
2021-05-29 00:13:39 -0400

    add sidebar, and chatbot for water and sleep

--- chat/chat.js
+++ chat/chat.js
@@ -10,6 +10,15 @@ https://goquotes.docs.apiary.io/#reference/list-all-datas/apiv1allquotes
 https://dmitripavlutin.com/javascript-fetch-async-await/
 */
 
+const wakeupBotMessage="Good Morning, Sunshine!"
+const sleepBotMessage="Okay! Have a good night! Hope you sleep well! ZzzZzz...";
+const waterBotMessage = (count) => {
+	if(count<4)
+		return 'Oh nooo. Please drink more to stay healthy!';
+	if(count<8)
+		return "That's better! You're almost there!"
+	return `That's great! ${count} glasses! Congratulations!!! You have consumed 8 glasses of water today! Please come back again tomorrow!`
+}
 console.log(`THE CHAT MODE IS: ${window.chatMode||'default'}`)
 
 const chatHistory = document.querySelector(".chat-history");
@@ -31,8 +40,35 @@ async function quote(){
 	return `${quotes[0].text}\n\n-- ${quotes[0].author}`;
 }
 
+function handleSleep(question){
+	if(question==="sleep:wakeup"){
+		addToChatHistory("I just woke up!", 'me');
+		//TODO: Send wake time to server
+		setTimeout(() => { 
+			addToChatHistory(wakeupBotMessage, 'bot');
+		}, 1000);
+	}
+	if(question==="sleep:sleep"){
+		addToChatHistory("I'm going to sleep!", 'me');
+		//TODO: Send sleep time to server
+		setTimeout(() => { 
+			addToChatHistory(sleepBotMessage, 'bot');
+		}, 1000);
+	}
+}
+
+function handleWater(question){
+	const [,glasses]=question.split(':')
+	addToChatHistory("I drink "+glasses, 'me');
+	setTimeout(() => { 
+		addToChatHistory(waterBotMessage(glasses), 'bot');
+	}, 1000);
+}
+
 let timer;
 async function askBot(question){
+	if(question.includes('sleep:'))return handleSleep(question)
+	if(question.includes('water:'))return handleWater(question)
 	const botMsg = `I don't know the answer...`;
 	const motivational = question.toLowerCase().includes('quote') && await quote();
 	if(timer) clearTimeout(timer);
@@ -42,7 +78,7 @@ async function askBot(question){
 }
 
 const input = document.querySelector(".input-box");
-input.addEventListener("keyup", function(event) {
+input && input.addEventListener("keyup", function(event) {
 	if (event.keyCode === 13) {
 		event.preventDefault();
 		addToChatHistory(input.value, 'me');
