dd4107aa936d5fc882e033b38857eec29ff6b6d2
crosshj <github@chimpjuice.com>
2021-05-31 06:38:29 -0400

    add server and database

--- index.js
+++ index.js
@@ -1,17 +1,33 @@
-function login(username, password) {
-	console.log({ username, password });
-	// fake login
+const userApiUrl = './api/users';
 
-	localStorage.setItem(
-		'credentials',
-		JSON.stringify({ username, password })
-	);
-
-	document.location = './login.html';
+const fetchJSON = (url) => fetch(url).then(x=>x.json());
+const postJSON = (url, body) => {
+	const opts = {
+		method: 'POST',
+		headers: {
+			'Content-Type': 'application/json'
+		},
+		body: JSON.stringify(body)
+	}
+	return fetch(url, opts).then(x=>x.json());
 }
 
-function clickHandler() {
-	console.log('click happened');
+const randomHash = () => (new Array(5)).fill().map(x => Math.random().toString(36).slice(2)).join('-');
+
+async function login(_username, _password) {
+	const [{
+		token, username, email, gender, dob
+	}={}]=[] = await fetchJSON(`${userApiUrl}?username=${_username}&password=${encodeURIComponent(_password)}`);
+	if(!token) return alert('Error logging in!');
+
+	localStorage.setItem(
+		'user',
+		JSON.stringify({ token, username, email, gender, dob })
+	);
+	document.location = './main/main.html';
+}
+
+window.clickHandler = () => {
 	const usernameInput = document.querySelector('#username');
 	const passwordInput = document.querySelector('#password');
 
@@ -21,18 +37,18 @@ function clickHandler() {
 	login(username, password);
 }
 
-function showSignup(){
+window.showSignup = () => {
 	document.querySelector('#sign-in').classList.add('hidden');
 	document.querySelector('#sign-up').classList.remove('hidden');
 }
 
-function createAccountClick(){
+window.createAccountClick = async () => {
 	const usernameInput = document.querySelector('#sign-up-username');
 	const passwordInput = document.querySelector('#sign-up-password');
 	const emailInput = document.querySelector('#sign-up-email');
 	const genderMale = document.querySelector('#sign-up-gender-male');
 	const birthdayInput = document.querySelector('#sign-up-birthday');
-	
+
 	const username = usernameInput.value;
 	const password = passwordInput.value;
 	const email = emailInput.value;
@@ -42,10 +58,11 @@ function createAccountClick(){
 
 	const birthday = birthdayInput.value;
 
-	const response = createAccount(
+	const response = await createAccount(
 		username, email, password, gender, birthday
 	);
-	
+	console.log(response);
+
 	if(response.success) return console.log('it worked');
 
 	const errorDiv = document.querySelector('.error');
@@ -57,17 +74,10 @@ const users = JSON.parse(
 	localStorage.getItem('users') || '[]'
 );
 
-function createAccount(username, email, password, gender, dob){
-	const found = users.find(x => x.username === username);
-	if(found) return { error: 'user already exists' };
-
-	users.push({ username, email, password, gender, dob });
-
-	// save new account to database
-	localStorage.setItem(
-		'users',
-		JSON.stringify(users)
-	);
-	
-	return { success: true };
+async function createAccount(username, email, password, gender, dob){
+	const body = {
+		username, email, password, gender, dob,
+		token: randomHash()
+	}
+	return await postJSON(userApiUrl, body);
 }
