090065a7c4e7ae50f87532b850d0a8011790f373
crosshj <github@chimpjuice.com>
2021-06-03 23:26:37 -0400

    fix menu for sub pages

--- main/main.js
+++ main/main.js
@@ -1,2 +1,15 @@
-// TODO: 
+// TODO:
 
+let open = false;
+const menuDiv = document.querySelector('.nav');
+const datContain = document.querySelector('.dat-contain');
+function toggleMenu(){
+    open = !open;
+    if(open){
+        menuDiv.classList.add('open');
+        datContain.classList.add('pushed')
+    } else {
+        menuDiv.classList.remove('open');
+        datContain.classList.remove('pushed')
+    }
+}
